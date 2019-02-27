e53ed4b922e38b56974d478be2ce546b7ff81d68
Jaromir Tomek <jaromir.tomek@gmail.com>
2019-02-27 13:26:39 +0100

    Fixing & testing

--- test/tc_vlnka.php
+++ test/tc_vlnka.php
@@ -17,10 +17,24 @@ class TcVlnka extends TcBase {
 		$output = smarty_block_vlnka($params,$content,$template,$repeat);
 		$this->assertEquals('<span title="Dr. Novák">Dr.~Novák</span> má čas <span title="v neděli">v~neděli</span>!',$output);
 
-		$ATK14_GLOBAL->lang = "en";
+		$content = '<strong>-30%</strong> Sleva';
+		$output = smarty_block_vlnka($params,$content,$template,$repeat);
+		$this->assertEquals('<strong>-30%</strong> Sleva',$output);
 
+		$content = 'v <strong>neděli</strong> bude krásně';
+		$output = smarty_block_vlnka($params,$content,$template,$repeat);
+		$this->assertEquals('v~<strong>neděli</strong> bude krásně',$output);
+
+		$content = 'v<strong>neděli</strong> bude krásně';
+		$output = smarty_block_vlnka($params,$content,$template,$repeat);
+		$this->assertEquals('v<strong>neděli</strong> bude krásně',$output);
+
+		// English
+		$ATK14_GLOBAL->lang = "en";
+		//
 		$content = '<span title="Dr. Novák">Dr. Novák</span> má čas <span title="v neděli">v neděli</span>!';
 		$output = smarty_block_vlnka($params,$content,$template,$repeat);
 		$this->assertEquals('<span title="Dr. Novák">Dr. Novák</span> má čas <span title="v neděli">v neděli</span>!',$output);
+
 	}
 }
